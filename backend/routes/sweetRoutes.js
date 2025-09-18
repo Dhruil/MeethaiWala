af9c6e359378dead879573c0e2487938e968433f
dhruil parmar <parmardhruil2611@gmail.com>
2025-09-18 15:54:32 +0530

    add route of search

--- backend/routes/sweetRoutes.js
+++ backend/routes/sweetRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { getAllSweets} from "../controllers/sweetController.js";
+import { getAllSweets, searchSweets} from "../controllers/sweetController.js";
 
 const router = express.Router();
 
 router.get("/", getAllSweets);
-
+router.get("/search", searchSweets);
 export default router;
