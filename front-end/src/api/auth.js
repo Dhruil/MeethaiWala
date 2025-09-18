a8d67bc6059760097732ff925afe6c42e3e5e7bd
dhruil parmar <parmardhruil2611@gmail.com>
2025-09-18 23:01:37 +0530

    Add authentication API with user registration and login functionality

---
+++ front-end/src/api/auth.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api/auth";
+
+
+// Register User/Owner
+export const registerUser = async (data) => {
+  try {
+    const res = await axios.post(`${API_URL}/register`, data);
+    return { success: true, token: res.data.token, message: res.data.message, user:res.data.user  };
+  } catch (error) {
+    return { success: false, message: error.response?.data?.message || "Error" };
+  }
+};
+
+// Login User/Owner
+export const loginUser = async (data) => {
+  try {
+    const res = await axios.post(`${API_URL}/login`, { ...data, type: data.role });
+    return { success: true, token: res.data.token, message: res.data.message, user: res.data.user  };
+  } catch (error) {
+    return { success: false, message: error.response?.data?.message || "Error" };
+  }
+};
