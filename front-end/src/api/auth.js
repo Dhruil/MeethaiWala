a068bd23c2749728359da7ba93f14632a12ff350
dhruil parmar <parmardhruil2611@gmail.com>
2025-09-19 18:27:06 +0530

    update url of api

--- front-end/src/api/auth.js
+++ front-end/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/auth";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 
 // Register User/Owner
@@ -16,7 +16,7 @@ export const registerUser = async (data) => {
 // Login User/Owner
 export const loginUser = async (data) => {
   try {
-    const res = await axios.post(`${API_URL}/login`, { ...data, type: data.role });
+    const res = await axios.post(`${API_URL}/auth/login`, { ...data, type: data.role });
     return { success: true, token: res.data.token, message: res.data.message, user: res.data.user  };
   } catch (error) {
     return { success: false, message: error.response?.data?.message || "Error" };
